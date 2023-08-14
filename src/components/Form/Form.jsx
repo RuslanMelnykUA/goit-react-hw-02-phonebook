3ecd0610e9728c3e0097444f239618b9a380b7c6
RuslanMelnykUA <indigo211212@gmail.com>
2023-08-14 23:30:33 +0300

    fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -41,7 +41,8 @@ export class Form extends Component {
                         type="text"
                         name="name"
                         value={name}
-                        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
                         onChange={this.onChangeInput}
                     />
@@ -52,7 +53,8 @@ export class Form extends Component {
                         type="tel"
                         name="number"
                         value={number}
-                        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"                        
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
                         onChange={this.onChangeInput}
                     />
